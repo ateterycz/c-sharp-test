0c10ccaaf4ce75939dcb46e6444b0f7e2420fa23
ateterycz <105075464+ateterycz@users.noreply.github.com>
2022-07-09 12:43:25 -0400

    Added files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LabApp.Models;
+
+namespace LabApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<LabApp.Models.Car> Car { get; set; }
+    }
+}
---
+++ Models/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabApp.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public int year { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+        public Car()
+        {
+
+        }
+    }
+    
+}
